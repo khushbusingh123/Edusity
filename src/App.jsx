827358a5495a90303d870a898db292cfa053dc88
khushbusingh123 <your-email@example.com>
2024-12-12 11:14:38 +0530

    Initial commit - Edusity React project

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import About from './Components/About/About'
-import Campus from './Components/Campus/Campus'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
-import Hero from './Components/Hero/Hero'
-import Navbar from './Components/Navbar/Navbar'
-import Programs from './Components/Programs/Programs'
-import Testimonials from './Components/Teatimonials/Testimonials'
-import Title from './Components/Title/Title'
- 
- const App = () => {
-   return (
-     <div>
-       <Navbar />
-       <Hero />
+// src/App.jsx
+import React from 'react';
 
-       <div className="container">
-       <Title subtitle='OUR PROGRAM' title='WHAT WE OFFER'/>
-       <Programs />
-       <About/>
-       <Title subtitle='Gallery' title='Campus photos'/>
-       <Campus/>
-       <Title subtitle='TESTIMONIALS' title='What Student Says'/>
+import Navbar from './Components/Navbar/Navbar';
+import Hero from './Components/Hero/Hero';
+import About from './Components/About/About';
+import Campus from './Components/Campus/Campus';
+import Programs from './Components/Programs/Programs';
+import Testimonials from './Components/Testimonials/Testimonials';
+import Contact from './Components/Contact/Contact';
+import Footer from './Components/Footer/Footer';
+import Title from './Components/Title/Title';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+
+      <div className="container">
+        <Title subtitle="OUR PROGRAM" title="WHAT WE OFFER" />
+        <Programs />
+
+        <About />
+
+        <Title subtitle="Gallery" title="Campus Photos" />
+        <Campus />
+
+        <Title subtitle="TESTIMONIALS" title="What Students Say" />
         <Testimonials />
-        <Title subtitle='CONTACT US' title='Get in Touch '/>
-        <Contact/>
-        <Footer/>
-       </div>
-             </div>
-   )
- }
- 
- export default App
- 
+
+        <Title subtitle="CONTACT US" title="Get in Touch" />
+        <Contact />
+
+        <Footer />
+      </div>
+    </div>
+  );
+};
+
+export default App;
--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,27 +1,24 @@
-import React from 'react'
-import './About.css'
-import about_img from '../../assets/about.png'
-import play_icon from '../../assets/play-icon.png'
+import React from 'react';
+import './About.css';
+import about_img from '../../assets/about.png';
+import play_icon from '../../assets/play-icon.png';
 
 const About = () => {
   return (
     <div className='about'>
-    <div className="about-left">
-        <img src = {about_img} alt="" className='about-img'/>
-        <img src = {play_icon} alt="" className='play-icon'/>
-    </div>
-    <div className="about-right">
+      <div className="about-left">
+        <img src={about_img} alt="About University" className='about-img' />
+        <img src={play_icon} alt="Play Icon" className='play-icon' />
+      </div>
+      <div className="about-right">
         <h3>ABOUT UNIVERSITIES</h3>
-        <h2>NOUTURING TOMOOROW LEADER TODAY</h2>
-        <p>Tjdlkjvklfdmkbjfdkkbpofkbo;kdf;lkb;okf;lbk;</p>
+        <h2>NURTURING TOMORROW'S LEADERS TODAY</h2>
+        <p>
+          Our universities are committed to providing a nurturing environment for the leaders of the future. With top-notch facilities and a focus on holistic development, we pave the way for success.
+        </p>
+      </div>
     </div>
-      
-    </div>
-  )
-}
- 
+  );
+};
 
-export default About
-
-
-
+export default About;
